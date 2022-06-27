4f23f5d956a496e71876a8d23476670c5cf787d7
axelmason <studpackerr@gmail.com>
2022-06-28 03:10:51 +0600

    Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var img = document.querySelector('.advice-block__button img');
+    $('.advice-block__button').on('click', function() {
+        $(this).find('img').addClass('spin');
+        fetch("https://api.adviceslip.com/advice")
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            $('.advice-block__num').html(data.slip.id);
+            $('.advice-block__text').html(data.slip.advice);
+        });
+    });
+    img.addEventListener('transitionend', e => {
+        img.classList.remove('spin');
+    })
+});
